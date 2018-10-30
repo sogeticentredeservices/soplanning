6e638e653a8303f05b46434ee5861e3cd9f6cf71
Cedric Bonhomme <bonhomme@neptune.one>
2018-10-30 17:30:20 +0100

    fix docker entrypoint, chown directory

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,14 @@ if [ "$1" == "/usr/bin/supervisord" ]; then
     if [ ! -e index.php ] && [ ! -e config.inc ]; then
         echo >&2 "SoPlanning not found in $PWD - copying now..."
         cp -r /usr/src/soplanning/* /var/www/html/
+        echo >&2 "fixing ownership..."
+        chmod -R 755 /var/www/html
+        chown -R www-data:www-data /var/www/html
         echo >&2 "Complete! SoPlanning has been successfully copied to $PWD"
         if [ ! -e "/var/www/html/database.inc" ] && [ ! -e "/var/www/html/config.inc" ]; then
             # Copy env variable to templates
             echo "Configuration files not found."
             echo "Generating configuration files..."
-            chmod -R 755 /var/www/html
-            chown -R www-data:www-data /var/www/html
         fi
     fi
 fi
