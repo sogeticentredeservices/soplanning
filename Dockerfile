7411f4c67e310a81c6797f4bbc0c98e1495fe154
Cedric Bonhomme <bonhomme@neptune.one>
2018-11-02 13:01:06 +0100

    Entrypoint fixed

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV PKG_VER=1-42
 
 # Installed required dependencies
 RUN apk add --no-cache --update \
-        unzip caddy supervisor openldap-dev libpng-dev
+        unzip caddy supervisor openldap-dev libpng-dev bash
 
 # Install php modules
 RUN docker-php-ext-install mysqli ldap gd pdo pdo_mysql
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
@@ -12,18 +12,18 @@ if [ "$1" == "/usr/bin/supervisord" ]; then
         rm /var/www/html/{config,database}.inc
         if [ ! -e "/var/www/html/database.inc" ] && [ ! -e "/var/www/html/config.inc" ]; then
             echo >&2 "Generating new configuration files..."
-            if [ -n "${SSO_PORTAL_URL+x}"]; then
+            if [ -n "${SSO_PORTAL_URL}" ]; then
                 sed -e "s#{{SSO_PORTAL_URL}}#${SSO_PORTAL_URL:-https://portal.example.com}#g" \
-                    /usr/src/config.inc.template > /var/www/config.inc
+                    /usr/src/config.inc.template > /var/www/html/config.inc
             else
                 echo >&2 "Missing environment variables!: SSO_PORTAL_URL"
             fi
-            if [ -n "${MYSQL_DATABASE+x}" ] && [ -n "${MYSQL_USER+x}" ] && [ -n "${MYSQL_PASSWORD+x}" ] && [ -n "${MYSQL_HOST+x}" ]; then
+            if [ -n "${MYSQL_DATABASE}" ] && [ -n "${MYSQL_USER}" ] && [ -n "${MYSQL_PASSWORD}" ] && [ -n "${MYSQL_HOST}" ]; then
                 sed -e "s#{{MYSQL_HOST}}#${MYSQL_HOST:-localhost}#g" \
                     -e "s#{{MYSQL_DATABASE}}#${MYSQL_DATABASE:-soplanning}#g" \
                     -e "s#{{MYSQL_USER}}#${MYSQL_USER}#g" \
                     -e "s#{{MYSQL_PASSWORD}}#${MYSQL_PASSWORD}#g" \
-                    /usr/src/database.inc.template > /var/www/database.inc
+                    /usr/src/database.inc.template > /var/www/html/database.inc
             else
                 echo >&2 "Missing environment variables!: MYSQL_HOST, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD"
             fi
