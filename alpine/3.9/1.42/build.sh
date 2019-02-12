dc1e69b9b9ced3fa46e89680d74e04d2c893e84e
Cedric Bonhomme <bonhomme@neptune.one>
2019-02-12 14:27:54 +0100

    Dropped alpine 3.8 -> 3.9

---
+++ alpine/3.9/1.42/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker build -t sogeti/soplanning:1.42-alpine -t sogeti/soplanning:1.42-alpine3.9 . "$@"
--- debian/stretch/1.43/build.sh
+++ debian/stretch/1.43/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker build -t sogeti/soplanning:1.43 . "$@"
+docker build -t sogeti/soplanning:latest -t sogeti/soplanning:1.43 -t sogeti/soplanning:1.43-stretch . "$@"
